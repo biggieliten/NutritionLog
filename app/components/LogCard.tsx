bdbd9232f622732c4d10118ade57d4818cfd8359
biggieliten <filipisaksson00@gmail.com>
2025-04-15 15:30:25 +0200

    LogCard structural change

--- app/components/LogCard.tsx
+++ app/components/LogCard.tsx
@@ -1,17 +1,33 @@
 import { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
-// import { GlobalContext } from "../state/GlobalState/GlobalContext";
 import { Log } from "../types/types";
 
 export const LogCard = (log: Log) => {
+  const orderedKeys: (keyof Log)[] = [
+    "calories",
+    "protein",
+    "carbohydrates",
+    "fat",
+    "sugar",
+    "fiber",
+  ];
+
   return (
     <View style={styles.container}>
-      {Object.entries(log).map(([key, value]) => (
-        <Text key={key} style={styles.text}>
-          {key.charAt(0).toUpperCase() + key.slice(1)}:{" "}
-          {value === log.date ? value : Math.round(value)}
-        </Text>
-      ))}
+      <Text style={styles.text}>{log.date}</Text>
+      {orderedKeys.map((key) => {
+        if (key in log) {
+          return (
+            <View key={key}>
+              <Text style={styles.text}>
+                {key.charAt(0).toUpperCase() + key.slice(1)}:{" "}
+                {Math.round(log[key] as number)}
+              </Text>
+            </View>
+          );
+        }
+        return null;
+      })}
     </View>
   );
 };
@@ -21,20 +37,23 @@ const styles = StyleSheet.create({
     margin: 10,
 
     height: "auto",
-    width: 250,
-
-    backgroundColor: "#91AC8F",
-    borderRadius: 15,
+    width: "85%",
+    borderBottomColor: "grey",
+    borderTopColor: "grey",
+    borderTopWidth: 0.5,
+    borderBottomWidth: 0.5,
+    // backgroundColor: "#91AC8F",
+    // borderRadius: 15,
 
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 2,
+    // shadowOpacity: 0.5,
+    // shadowRadius: 3.84,
+    // elevation: 2,
   },
   text: {
     fontSize: 20,
     fontWeight: "bold",
-    color: "white",
+    color: "grey",
   },
 });
