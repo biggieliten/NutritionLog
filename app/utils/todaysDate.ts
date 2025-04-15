d2d2276c85699d7d3c56c2c88e0ed072fb1ea200
biggieliten <filipisaksson00@gmail.com>
2025-04-15 15:30:03 +0200

    add date formatter

--- app/utils/todaysDate.ts
+++ app/utils/todaysDate.ts
@@ -1,1 +1,8 @@
 export const getToday = () => new Date().toISOString().split("T")[0];
+
+export const formatDate = (dateString: string): string => {
+  const date = new Date(dateString);
+  return (
+    date.getDate() + " " + date.toLocaleString("default", { month: "long" })
+  );
+};
